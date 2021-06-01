eed16451d06c9a5902ef3cca7fda815a14107465
Stephen Steinkirch <ssteinkirch@productiveedge.com>
2021-06-01 17:05:36 -0500

    Reciever Re-do

--- r2r.py
+++ r2r.py
@@ -12,13 +12,31 @@ import RPi.GPIO as GPIO
 
 beep = None
 
-class Beeper(triggers.trigger, object):
-        timestamp = None
-	expiration = 0
+class Sprinkler(triggers.trigger, object):
+    timestamp = None    
+    
+    def __init__(self, trigger_name_in, GPIO_Pin_in, expiration):
+        self.expiration = expiration
+        super(Beeper, self).__init__(trigger_name_in, GPIO_Pin_in)
 
-        def __init__(self, trigger_name_in, GPIO_Pin_in, expiration):
-		self.expiration = expiration
-		super(Beeper, self).__init__(trigger_name_in, GPIO_Pin_in)
+    def on(self, on_seconds=60):
+        triggers.trigger.on(self)
+        self.expiration = on_seconds
+        self.timestamp = time.time()
+
+    def check_expire(self):
+        if self.status() and (time.time() - self.timestamp) > self.expiration:
+            triggers.trigger.off(self)
+            return True
+        return False
+
+
+class Beeper(triggers.trigger, object):
+    timestamp = None
+    
+    def __init__(self, trigger_name_in, GPIO_Pin_in, expiration):
+        self.expiration = expiration
+        super(Beeper, self).__init__(trigger_name_in, GPIO_Pin_in)
 
 	def on(self):
 		triggers.trigger.on(self)
@@ -29,7 +47,15 @@ class Beeper(triggers.trigger, object):
 			triggers.trigger.off(self)
 			return True
 		return False
-
+        
+    def beeps(self, number_of_beeps):
+        for i in range(0,number_of_beeps):
+            if i !=0:
+                sleep(0.2)
+            triggers.trigger.on(self)
+            sleep(0.2)
+            triggers.trigger.off(self)
+                
 
 
 class Receiver():
@@ -41,9 +67,9 @@ class Receiver():
 	beep = None
 
 
-	def __init__(self, pin, relay, beep):
+	def __init__(self, pin, sprink, beep):
 		self.pin = pin
-		self.relay = relay
+		self.sprink = sprink
 		self.beep = beep
 
 	def reset(self):
@@ -53,96 +79,78 @@ class Receiver():
 			
 	def check(self):
 		now = time.time()
-		if self.pin.check():  #If button down.
+        self.sprink.check_expire()
+        
+		if self.pin.check():  # If button down.
 			if not self.on:
 				self.click_count += 1
 				self.on = True
 				self.timestamp = now
 				print("click " + self.pin.sensor_name)
-				self.beep.on()
-
-		elif self.on:  #If button is not down, but it used to be the last time we checked.
+				self.beep.beeps(1)
+		elif self.on:  # If button is not down, but it used to be the last time we checked.
 			self.on = False
-			#print("let go")
 		elif self.click_count > 0:
-			#print("time now: " + str(now))
-			#print("timestamp: " + str(self.timestamp))
-			#print("click count: " + str(self.click_count))
-			if (now - self.timestamp) >= 2:  #If the last time we clicked was greater than two seconds 
-				#print("It's been two seconds")
+			if (now - self.timestamp) >= 2:  # If the last time we clicked was greater than two seconds 
 				if self.click_count == 1:
-					if self.relay.status():
-						self.relay.off()
-                                                print(self.relay.status())
-						self.beep.off()
-					else:
-						self.relay.on()
-                                                print(self.relay.status())
+					if self.sprink.status():
+						self.sprink.off()
+                        print(self.sprink.status())
+                        self.beep.beeps(1)
+                    else:
+                        self.spring.on(300)
+                        self.beep.beeps(2)
+                        print(">1")
 
-				elif self.click_count == 2:
+				elif self.click_count == 2 and not self.sprink.status():
+                    self.spring.on(600)
+                    self.beep.beeps(4)
 					print(">2")
-				elif self.click_count == 3:
+				elif self.click_count == 3 and not self.sprink.status():
+                    self.spring.on(1800)
+                    self.beep.beeps(4)
 					print(">3")
 				self.reset()
-			#else:  #If the last time we clicked was less than two seconds
-				#print("It's been LESS THAN two seconds")
+			
 
 
 def loop():
-	GPIO.cleanup()
-	print("Started\n")
-
-	relay1 = triggers.trigger('One',21)
-	relay2 = triggers.trigger('Two',20)
-	relay3 = triggers.trigger('Three',16)
-	relay4 = triggers.trigger('Four',12)
-
-	beep = Beeper('Beep',24,1)
-
-	pin1 =  sensors.sensor("One", 14)
-	pin2 =  sensors.sensor("Two", 15)
-	pin3 =  sensors.sensor("Three", 18)
-	pin4 =  sensors.sensor("Four", 23)
+    GPIO.cleanup()
+    print("Started\n")
+    
+    sprink1 = triggers.trigger('One', 21)
+    sprink2 = triggers.trigger('Two', 20)
+    sprink3 = triggers.trigger('Three', 16)
+    sprink4 = triggers.trigger('Four', 12)
+    
+    beep = Beeper('Beep', 24, 1)
+    
+    pin1 = sensors.sensor("One", 14)
+    pin2 = sensors.sensor("Two", 15)
+    pin3 = sensors.sensor("Three", 18)
+    pin4 = sensors.sensor("Four", 23)
 	
-	r1 = Receiver(pin1, relay1, beep)
-	r2 = Receiver(pin2, relay2, beep)
-	r3 = Receiver(pin3, relay3, beep)
-	r4 = Receiver(pin4, relay4, beep)
-
-	print('Beep On')
-	#GPIO.output(24, GPIO.HIGH)
-	beep.on()
-	#while not beep.check_expire():
-	#	time.sleep(1)
-	#	print('!')
-	
-
-	#GPIO.output(24, GPIO.HIGH)
-	#beep.off()
-	#print('Beep Off')
-	
-
-	try:
+    r1 = Receiver(pin1, sprink1, beep)
+    r2 = Receiver(pin2, sprink2, beep)
+    r3 = Receiver(pin3, sprink3, beep)
+    r4 = Receiver(pin4, sprink4, beep)
+    
+    try:
 		while True:
 			r1.check()
-			beep.check_expire()
 			r2.check()
-			beep.check_expire()
 			r3.check()
-			beep.check_expire()
 			r4.check()
-			beep.check_expire()
-	except KeyboardInterrupt:
-    		pass
+    except KeyboardInterrupt:
+        pass
+    
+    try:		
+        GPIO.cleanup()
+        print("Clean Exit")
+    except:
+        pass	
 
-	GPIO.cleanup()	
 
 if __name__ == '__main__':
 	loop()
-
-
-
-
-
-
 
